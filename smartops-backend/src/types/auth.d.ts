feeb7717c0a9de73004ac3fd44332dbc5d5214fd
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2025-12-09 14:51:30 +0530

    fresh backend file structure

--- smartops-backend/src/types/auth.d.ts
+++
@@ -1,9 +0,0 @@
-// src/types/auth.ts
-
-/**
- * Defines the structure of the data stored in the JWT payload and attached to req.user.
- */
-export interface AuthPayload {
-    userId: string;
-    sessionId: string;
-}
